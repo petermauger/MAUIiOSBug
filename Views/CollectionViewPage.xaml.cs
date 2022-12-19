98437ddf4687ad629d28a473bdbca694cbacebd7
Peter Mauger <peter@norwoodtech.com.au>
2022-12-19 21:52:56 +0800

    Adding repro for collectionview bug for iPhone 14 Pro devices

---
+++ Views/CollectionViewPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace iOSCellReuseBug.Views;
+
+public partial class CollectionViewPage : ContentPage
+{
+    public List<CellViewModel> Cells { get; }
+
+    public CollectionViewPage()
+	{
+		Cells = new List<CellViewModel>();
+        for (int i = 0; i < 1000; i++)
+        {
+            if (Random.Shared.NextDouble() >= 0.5)
+                Cells.Add(new CellViewModel());
+            else
+                Cells.Add(new ScrollableCellViewModel());
+        }
+        InitializeComponent();
+        list.BindingContext = this;
+    }
+}
